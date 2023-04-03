b90f9ad6679d2682dc94e09e9b4b0b0a710c7b8a
JustinMitchell <mitchelljm04@gmail.com>
2023-04-03 14:20:40 -0400

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+
+
+Post.create!( title: "Palm Beach", description: "Austrlia has some of the most beautiful beaches you can visit. This beach is on the northern end of NSW in a town called Palm Beach. At the top of the cliff there is a Lighthouse where you can see across all of the beach and the surrounding town. The ligthouse itself is called Barrenjoey Ligthhouse.",category: "Beach", image: "./Images/Lighthouse.jpg")
+Post.create!( title: "Elvina Bay", description: "This was an awesome trip that we had to one of my friends houses. We had to take a taxi to an island. The sunset was amazing here and the views were even better.", category: "Beach", image: "./Images/elvinabay.jpg")
